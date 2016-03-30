b6969426f1c518f58d7e437e557629dd1a00fd0b
demohi <mds@xue.bi>
2016-03-30 17:15:03 +0800

    bump 0.2.1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const surg = require('surg');
 const convert = require('./lib/convert');
-module.exports = config => {
+const md5 = require('./lib/md5');
+module.exports = (config, name, version) => {
+  version = version || md5(config);
   const surge = surg(config);
-  return convert(surge);
+  return convert(surge, name, version);
 };
